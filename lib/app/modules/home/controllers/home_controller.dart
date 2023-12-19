fe92abd55729fc18beeff24d89c24bed16a174b8
aldianprawira <aldianprawira99@gmail.com>
2023-12-19 17:46:40 +0700

    [UPDATE 12]-Filter video di home

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,7 +5,7 @@ class HomeController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<QuerySnapshot<Map<String, dynamic>>> getData() async {
-    var listData = firestore.collection("videos").where("Category", isEqualTo: "Science").orderBy("CreatedAt").get();
+    var listData = await firestore.collection("videos").orderBy("CreatedAt").get();
     return listData;
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,7 +6,6 @@ import 'package:konekin/app/routes/app_pages.dart';
 import '../../../shared/theme/color.dart';
 import '../../../shared/widget/logo_konekin_outlined.dart';
 import '../../../shared/widget/primary_card.dart';
-import '../../../shared/widget/recommendation_card.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -32,89 +31,9 @@ class HomeView extends GetView<HomeController> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const SizedBox(height: 16),
-                    RichText(
-                      text: TextSpan(
-                        text: "Top courses in ",
-                        style: TextStyle(
-                          color: black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "Science",
-                            style: TextStyle(
-                              color: primary,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    SizedBox(
-                      height: 244,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: listAllDocs.length,
-                        itemBuilder: (context, index) {
-                          Map<String, dynamic> data = listAllDocs[index].data();
-                          if (data["Category"] == "Science") {
-                            return PrimaryCard(
-                              onTap: () => Get.toNamed(Routes.VIDEO_PLAYER, arguments: data["VideoURL"]),
-                              image: data["Thumbnail"],
-                              title: data["Title"],
-                              creator: data["Creator"],
-                              price: data["Price"],
-                              bestSeller: true,
-                            );
-                          } else {
-                            return const SizedBox();
-                          }
-                        },
-                      ),
-                    ),
+                    _buildCourseList("Science", listAllDocs),
                     const SizedBox(height: 16),
-                    RichText(
-                      text: TextSpan(
-                        text: "Top courses in ",
-                        style: TextStyle(
-                          color: black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "Science",
-                            style: TextStyle(
-                              color: primary,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    SizedBox(
-                      height: 244,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: listAllDocs.length,
-                        itemBuilder: (context, index) {
-                          var data = listAllDocs[index].data();
-                          return PrimaryCard(
-                            onTap: () => Get.toNamed(Routes.VIDEO_PLAYER, arguments: data["VideoURL"]),
-                            image: data["Thumbnail"],
-                            title: data["Title"],
-                            creator: data["Creator"],
-                            price: data["Price"],
-                            bestSeller: true,
-                          );
-                        },
-                      ),
-                    )
+                    _buildCourseList("Design", listAllDocs),
                   ],
                 ),
               ),
@@ -124,6 +43,59 @@ class HomeView extends GetView<HomeController> {
           }
         },
       ),
+    );
+  }
+
+  // Extract Method
+  // Create a course list if listAllDosc with certain categories exist
+  Widget _buildCourseList(String category, List<QueryDocumentSnapshot<Map<String, dynamic>>> listAllDocs) {
+    var filteredDocs = listAllDocs.where((element) => element.data()["Category"] == category).toList();
+    if (filteredDocs.isEmpty) {
+      return const SizedBox();
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        RichText(
+          text: TextSpan(
+            text: "Top courses in ",
+            style: TextStyle(
+              color: black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: category,
+                style: TextStyle(
+                  color: primary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        SizedBox(
+          height: 244,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: filteredDocs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = filteredDocs[index].data();
+              return PrimaryCard(
+                onTap: () => Get.toNamed(Routes.VIDEO_PLAYER, arguments: data["VideoURL"]),
+                image: data["Thumbnail"],
+                title: data["Title"],
+                creator: data["Creator"],
+                price: data["Price"],
+                bestSeller: true,
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
